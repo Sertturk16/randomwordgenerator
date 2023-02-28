8bcd8f573d3e9c9638cf23a9c9c5e6423d1fca1b
Sertturk16 <88446918+Sertturk16@users.noreply.github.com>
2023-02-28 11:40:45 +0300

    deploy

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,82 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <template v-if="!submitted">
+          <v-textarea
+            :flat="true"
+            label="Enter Words"
+            class="words-textarea"
+            v-model="words"
+            color="primary"
+            solo
+            bg-color="primary"
+          ></v-textarea>
+          <v-btn class="primary" @click="saveWords" :disabled="!words.length"
+            >Save Words</v-btn
+          >
+        </template>
+        <template v-else>
+          <div>
+            <div class="word-card-wrapper text-center">
+              <v-alert color="primary" prominent style="color: #fff">
+                {{ currentWord }}
+              </v-alert>
+            </div>
+            <div class="text-center mb-2">
+              <span>{{ wordsArr.length }} words left.</span>
+            </div>
+            <div class="d-flex justify-center">
+              <v-btn
+                class="primary mr-2"
+                :disabled="!this.wordsArr.length"
+                @click="generateNewWord()"
+                >Next</v-btn
+              >
+              <v-btn @click="restart()" class="primary">Restart</v-btn>
+            </div>
+          </div>
+        </template>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data: () => ({
+    words: "",
+    nextDisable: false,
+    currentWord: null,
+    submitted: false,
+    wordsArr: [],
+    poppedWordsArr: [],
+  }),
+  methods: {
+    saveWords() {
+      this.wordsArr = this.words.split(",");
+      this.submitted = true;
+      this.generateNewWord();
+    },
+    generateNewWord() {
+      let i = Math.floor(Math.random() * this.wordsArr.length);
+      this.currentWord = this.wordsArr.splice(i, 1)[0];
+      this.poppedWordsArr.push(this.currentWord);
+    },
+    restart() {
+      this.nextDisable = false;
+      this.currentWord = null;
+      this.submitted = false;
+      this.wordsArr = [];
+      this.poppedWordsArr = [];
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+.words-textarea .v-input__slot {
+  background: #e3f2fd !important;
 }
 </style>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+import 'vuetify/dist/vuetify'
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  ssr: true
+});
